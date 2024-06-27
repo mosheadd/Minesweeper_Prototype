9d6ce76b3fbd16c74f940de628401433b078cdba
mosheadd <rost1234567890qgp@gmail.com>
2024-06-27 16:03:10 +0300

    Add Map.h

---
+++ Project3/Map.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+
+namespace mscp
+{
+
+	constexpr auto UNCOVER = 1;
+	constexpr auto FLAG = 2;
+	constexpr auto EXIT = 3;
+
+	void clearWindow();
+
+	int getNumberInRange(int min_val, int max_val);
+
+	class Map
+	{
+		std::vector<std::vector<short>> map;
+		short height, width;
+
+		enum
+		{
+			COVERED = 0,
+			UNCOVERED = 1,
+			FLAGGED = 2,
+			MINE = 3,
+			FLAGGED_MINE = 4,
+		};
+
+	public:
+		Map();
+		Map(short height, short width);
+
+		~Map();
+
+		void makeRoad(short row, short col);
+		void getMinesCount();
+		void gameloop();
+
+		bool checkWinCondition();
+
+	};
+}
