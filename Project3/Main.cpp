b5efe45ac38d44e164cec0a73bae2cd07a1eee7a
mosheadd <rost1234567890qgp@gmail.com>
2024-06-27 15:49:02 +0300

    Добавьте файлы проекта.

---
+++ Project3/Main.cpp
@@ -0,0 +1,298 @@
+#define UNCOVER 1
+#define FLAG 2
+#define EXIT 3
+
+#include "iostream"
+#include "Windows.h"
+#include <vector>
+
+using namespace std;
+
+vector<vector<short>> map;
+
+enum
+{
+	COVERED = 0,
+	UNCOVERED = 1,
+	FLAGGED = 2,
+	MINE = 3,
+	FLAGGED_MINE = 4,
+};
+
+void clearWindow()
+{
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo(console, &screen);
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition(console, topLeft);
+}
+
+int getNumberInRange(int min_val, int max_val) { return min_val + rand() % max_val; }
+
+int getBombsCount(short row, short col)
+{
+	int bombs_count = 0;
+
+	if (row >= 1 && row <= 8 && col >= 1 && col <= 8)
+	{
+		for (int t = row - 1; t < row + 2; t++)
+			for (int k = col - 1; k < col + 2; k++)
+				bombs_count += map[t][k] == MINE || map[t][k] == FLAGGED_MINE;
+	}
+	else if (col >= 1 && col <= 8)
+	{
+
+		if (row == 0)
+		{
+			for (int t = 0; t < 2; t++)
+				for (int k = col - 1; k < col + 2; k++)
+					bombs_count += map[t][k] == MINE || map[t][k] == FLAGGED_MINE;
+		}
+		else
+		{
+			for (int t = 8; t < 10; t++)
+				for (int k = col - 1; k < col + 2; k++)
+					bombs_count += map[t][k] == MINE || map[t][k] == FLAGGED_MINE;
+		}
+	}
+	else if (row >= 1 && row <= 8)
+	{
+		if (col == 0)
+		{
+			for (int t = row - 1; t < row + 2; t++)
+				for (int k = 0; k < 2; k++)
+					bombs_count += map[t][k] == MINE || map[t][k] == FLAGGED_MINE;
+		}
+		else
+		{
+			for (int t = row - 1; t < row + 2; t++)
+				for (int k = 8; k < 10; k++)
+					bombs_count += map[t][k] == MINE || map[t][k] == FLAGGED_MINE;
+		}
+	}
+	else
+	{
+		short coeff1 = (row == 0) - (row == 9);
+		short coeff2 = (col == 0) - (col == 9);
+		bombs_count += map[row + coeff1][col + coeff2] == MINE + map[row][col + coeff2] == MINE + map[row + coeff1][col] == MINE;
+		bombs_count += map[row + coeff1][col + coeff2] == FLAGGED_MINE + map[row][col + coeff2] == FLAGGED_MINE + map[row + coeff1][col] == FLAGGED_MINE;
+	}
+
+	return bombs_count;
+
+}
+
+void makeRoad(short row, short col)
+{
+
+	if (row < 0 || row > 9 || col < 0 || col > 9 || map[row][col] == UNCOVERED|| map[row][col] == MINE || map[row][col] == FLAGGED_MINE) return;
+
+	int cell_bombs_count = getBombsCount(row, col);
+
+	map[row][col] = UNCOVERED;
+
+	if (cell_bombs_count > 0) return;
+
+	makeRoad(row - 1, col - 1);
+	makeRoad(row - 1, col);
+	makeRoad(row - 1, col + 1);
+	makeRoad(row, col - 1);
+	makeRoad(row, col + 1);
+	makeRoad(row + 1, col - 1);
+	makeRoad(row + 1, col);
+	makeRoad(row + 1, col + 1);
+
+}
+
+bool checkGameWinCondition()
+{
+	for (auto i : map)
+	{
+		for (auto j : i)
+		{
+			if (j == COVERED)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+class Map
+{
+	vector<vector<short>> map;
+	short height, width;
+
+public:
+	Map()
+	{
+		height = 10;
+		width = 10;
+	}
+	Map(short height, short width)
+	{
+		this->height = height;
+		this->width = width;
+	}
+
+	~Map()
+	{
+
+	}
+
+	void gameloop()
+	{
+
+		short choose, row, col;
+		bool exitGame = false;
+
+	}
+
+};
+
+int main()
+{
+	srand(time(0));
+
+	short choose, row, col;
+	bool exitGame = false;
+
+	cin >> choose;
+
+	if (choose == 2) exit(0);
+
+	cin >> row >> col;
+
+	for (short i = 0; i < 10; i++)
+	{
+		map.push_back({});
+		for (short j = 0; j < 10; j++)
+		{
+			if (row == i + 1 && col == j + 1)
+				map[i].push_back(COVERED);
+			else
+			{
+				int rn = getNumberInRange(0, 100);
+				if (rn > 15) map[i].push_back(COVERED);
+				else map[i].push_back(MINE);
+			}
+		}
+	}
+
+	makeRoad(row - 1, col - 1);
+
+	clearWindow();
+
+	while (true)
+	{
+		for (short i = 0; i < 10; i++)
+		{
+			for (short j = 0; j < 10; j++)
+			{
+				if (map[i][j] == COVERED || map[i][j] == MINE) cout << "*";
+				else if (map[i][j] == FLAGGED || map[i][j] == FLAGGED_MINE) cout << "!";
+				else if (map[i][j] == UNCOVERED)
+				{
+					int bombs_count = getBombsCount(i, j);
+
+					if (bombs_count == 0)
+						cout << "-";
+					else
+						cout << bombs_count;
+				}
+			}
+			cout << endl;
+		}
+
+		cin >> choose;
+
+		switch (choose)
+		{
+		case EXIT:
+			exitGame = true;
+			break;
+		case UNCOVER:
+			cin >> row >> col;
+
+			if (map[row - 1][col - 1] == MINE || map[row - 1][col - 1] == FLAGGED_MINE)
+			{
+				clearWindow();
+				cout << "lol you died" << endl;
+				exitGame = true;
+			}
+			else
+			{
+				makeRoad(row - 1, col - 1);
+			}
+			break;
+		case FLAG:
+			cin >> row >> col;
+
+			switch (map[row - 1][col - 1])
+			{
+			case UNCOVERED:
+				map[row - 1][col - 1] = FLAGGED;
+				break;
+			case MINE:
+				map[row - 1][col - 1] = FLAGGED_MINE;
+				break;
+			case FLAGGED:
+				map[row - 1][col - 1] = UNCOVERED;
+				break;
+			case FLAGGED_MINE:
+				map[row - 1][col - 1] = MINE;
+				break;
+			default:
+				break;
+			}
+
+			break;
+		default:
+			break;
+		}
+
+		if (exitGame)
+			break;
+
+		clearWindow();
+
+		bool game_won = checkGameWinCondition();
+
+		if (game_won)
+		{
+			cout << "you won\n";
+			break;
+		}
+
+	}
+
+	for (short i = 0; i < 10; i++)
+	{
+		for (short j = 0; j < 10; j++)
+		{
+			if (map[i][j] == MINE || map[i][j] == FLAGGED_MINE) cout << "+";
+			else
+			{
+				int bombs_count = getBombsCount(i, j);
+
+				if (bombs_count == 0)
+					cout << "-";
+				else
+					cout << bombs_count;
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
